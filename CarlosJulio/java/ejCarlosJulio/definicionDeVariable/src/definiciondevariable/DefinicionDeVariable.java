e242350937f4370e1a9a1597ed37899a86d854dd
camiloMotta <camilomota44@gmail.com>
2024-06-27 20:41:17 -0500

    carlos

---
+++ CarlosJulio/java/ejCarlosJulio/definicionDeVariable/src/definiciondevariable/DefinicionDeVariable.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package definiciondevariable;
+
+/**
+ *
+ * @author SENA
+ */
+public class DefinicionDeVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num;
+        String nombre;
+        double decimal;
+        boolean boleano;
+        float flotante;
+        // asignacion 
+        nombre="carlos";
+        num=4;
+        decimal=10.5;
+        boleano=true;
+        flotante=5.3f;
+        //imprimir 
+        System.out.println(num);
+        System.out.println(nombre);
+        System.out.println(decimal);
+        System.out.println(boleano);
+        System.out.println(flotante);
+        
+        
+        int num1;
+        int num2;
+        int num3=30;
+        int suma;
+        int resta;
+        num1=34;
+        num2=6;
+        suma=num1+num2;
+        resta=suma-num3;
+ 
+        System.out.println("suma de= 34+6");
+        System.out.println(suma);
+        System.out.println("resultado resta= 40-30 ");
+        System.out.println(resta);
+        
+    }
+    
+}
---
+++ CarlosJulio/java/ejCarlosJulio/helloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworld;
+
+/**
+ *
+ * @author CAMILO_MOTTA
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        system=sistema
+        out= salida
+        print=imprimir 
+        println= imprimir con salto de linea 
+        ("mensaje")
+        ej 
+        System.out.print("mensaje);
+        */
+        System.out.println("Hola mundo ");
+        System.out.println("Como van sus dias mis amores??? ");
+        System.out.println("Hola mundo ");
+    }
+    
+}
